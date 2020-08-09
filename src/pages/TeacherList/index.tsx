5c1a7b2d980d9f02a244f6a337f94e226e53cd57
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2020-08-09 19:07:40 -0300

    Dia 5 do NLW - Final

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,15 +1,123 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {View, ScrollView, Text} from 'react-native';
+
+import api from '../../services/api';
 
 import styles from './style';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, {Teacher} from '../../components/TeacherItem';
+import { TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import {Feather} from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
 
 function TeacherList() {
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  function loadFavorites() {    
+    AsyncStorage.getItem('favorites').then(response => {
+      if(response) {    
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+          return teacher.id;
+        })
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
+
+
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(!isFiltersVisible);
+  }
+
+  const handleFiltersSubmit = async() => {
+    loadFavorites();
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time,
+      }
+    });
+    setIsFiltersVisible(false);
+    setTeachers(response.data);
+  }
+
   return (
   <View style={styles.container}>
     <PageHeader
       title="Proffys disponíveis"
-    />
+      headerRight={(
+        <BorderlessButton onPress={handleToggleFiltersVisible}>
+          <Feather name="filter" size={20} color='#FFF'/>
+        </BorderlessButton>
+      )}
+    >
+      { isFiltersVisible && (
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Qual a matéria?"   
+            value={subject}
+            onChangeText={text => setSubject(text)}
+            placeholderTextColor="#c1bccc"
+          />
+
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da Semana</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual o dia?"
+                value={week_day}
+                onChangeText={text => setWeekDay(text)}
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual horário?"
+                value={time}
+                onChangeText={text => setTime(text)}
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+          </View>
+
+          <RectButton onPress={handleFiltersSubmit} style={styles.submitButton}>
+            <Text style={styles.submitButtonText}>Filtrar</Text>
+          </RectButton>
+
+        </View>
+      )}
+    </PageHeader>
+
+    <ScrollView
+      style={styles.teacherList}
+      contentContainerStyle={{
+        paddingHorizontal: 16,
+        paddingBottom: 16,
+      }}
+    >
+      {teachers.map((teacher: Teacher) => {
+        return (
+        <TeacherItem 
+          key={teacher.id} 
+          teacher={teacher}
+          favorited={favorites.includes(teacher.id)}/>
+        )
+      })}
+    </ScrollView>
   </View>
   )
 }
